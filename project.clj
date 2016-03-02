b2c39adad85bcd56c0aed1bd045d2f1373ea273d
Kunal Nawale <knawale@yieldbot.com>
2016-03-02 00:59:07 -0500

    update torna version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [yieldbot/torna "0.1.0-SNAPSHOT"]
+                 [yieldbot/torna "0.1.6-SNAPSHOT"]
                  [yieldbot/prismo "0.2.0-SNAPSHOT"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.cli "0.3.1"]]
